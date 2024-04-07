1e1e59d1a64cc4ecc61f1076b7efa635e9d5f105
test <test@test.local>
2024-04-07 15:26:47 +0300

    modified:   [RunAsAdmin]AddVariablesAndCopyFiles.cmd

--- [RunAsAdmin]AddVariablesAndCopyFiles.cmd
+++ [RunAsAdmin]AddVariablesAndCopyFiles.cmd
@@ -1,12 +1,12 @@
 chcp 65001 
 REM Путь до папки в которую будут скопированы файлы
-set pathAndroidStudio="C:\AndroidSettings\"
+set pathAndroidStudio=C:\AndroidSettings\
 
 REM Копируем папки\файлы на диск C:\
-robocopy "%~dp0.android" %pathAndroidStudio%.android\ /z /eta /compress /mir
-robocopy "%~dp0Sdk" %pathAndroidStudio%Sdk\ /z /eta /compress /mir
-robocopy "%~dp0Gradle" %pathAndroidStudio%Gradle\ /z /eta /compress /mir
-robocopy "%~dp0init.d" %pathAndroidStudio%Gradle\init.d\ /z /eta /compress /mir
+robocopy "%~dp0.android" "%pathAndroidStudio%.android\" /z /eta /compress /mir
+robocopy "%~dp0Sdk" "%pathAndroidStudio%Sdk\" /z /eta /compress /mir
+robocopy "%~dp0Gradle" "%pathAndroidStudio%Gradle\" /z /eta /compress /mir
+robocopy "%~dp0init.d" "%pathAndroidStudio%Gradle\init.d\" /z /eta /compress /mir
 
 REM Прописываем переменные в систему
 pushd "%~dp0" 
